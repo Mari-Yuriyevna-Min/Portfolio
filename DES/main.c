fe50c9b6621b8bcd4df3039684e89097adf9a1be
Mari <mari0.0yuriyevna@gmail.com>
2025-07-12 18:49:17 +0900

    Update main.c

--- DES/main.c
+++ DES/main.c
@@ -9,7 +9,9 @@
 void compared(int ret);
 int password(char pass0[6], char pass1[6]);
 char plain(char message0[321], int message1[321], int gett); 
-void returntobinary(int message1[321], int binary0[257][10]);
+int returntobinary(int message1[321], int binary0[257][10]);
+
+
 
 int main()
 {
@@ -37,6 +39,7 @@ int main()
 	plain(message0, message1, gett);
 	returntobinary(message1,binary0);
 }
+
 int password(char pass0[6], char pass1[6])
 {
 	int i = 0;
@@ -58,6 +61,7 @@ int password(char pass0[6], char pass1[6])
 	return strcmp(pass0, pass1);
 
 }
+
 void compared(int ret)
 {
 	if (ret == 0)
@@ -72,6 +76,7 @@ void compared(int ret)
 		exit(0);
 	}
 }
+
 char plain(char message0[321], int message1[321], int gett)
 {
 	printf("\n >> Now, Enter your message in 320 charactres!");
@@ -88,36 +93,45 @@ char plain(char message0[321], int message1[321], int gett)
 		}
 		message0[j++] = gett;
 	}
-	message0[j] = '\0';
-	for (int i = 320;i < message0[j];i--)
+	if (gett > 320)
 	{
-		message0[i] == 0;
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
+		printf("\n >> The character limit of 320 has been exceeded... The program will now terminate...");
+		return 0;
 	}
+	message0[j] = '\0';
 	printf("\n >> GOOD JOB :)\n");
-	for (int k = 0;k < 320;k++)
+	for (int k = 0; k < 320; k++)
 	{
 		message1[k] = message0[k];
-	}
+	} // 여기서 0으로 320자 모두 채워진 message0 을 message1(char)으로 가져다 넘기기.
 }
-void returntobinary(int message1[321], int binary0[257][10])
+
+int returntobinary(int message1[321], int binary0[257][10])
 {
+	int binarysize = (sizeof(message1));
+	
 	int i = 0;
 	int j = 0;
 	int k = 0;
-	for (i = 0; i < 320; i++)
+	for (i = 0; i < binarysize; i++) // 320자 중에 들어온 문자열 크기 세어서
 	{
-		char c = message1[i];
-		for (int b = 7; b >= 0; b--)
+		char c = message1[i]; 
+		for (int b = 7; b >= 0; b--) // binary0 배열에 2진수로 바꿔서 넣는 거
 		{
-			binary0[j][k] = (c >> b) & 1;
+			binary0[j][k] = (c >> b) & 1; // 2진수 바꾸기
 			k++;
-			if (k == 10)
+			if (k == 10) // 만약에 10번째 열까지 채워졌다면
 			{
-				k = 0;
+				k = 0; // 
 				j++;
-				if (j == 257) break;
+				if (j == 256) break; // 256번째 행에 넣고 그만 넣기
 			}
 		}
+		
+		int modbinary = (320 - binarysize);
+		
 	}	
 }
+
 
